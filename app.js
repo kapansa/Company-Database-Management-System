d976160a798884dff471c12137015d799118c11b
kapansa <samuelkapansa@gmail.com>
2021-06-23 15:25:00 +0200

    Update server

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
+require("dotenv").config;
 const express = require("express");
-const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
+mongoose.connect(`${process.env.DB_ACCESS}`, {useNewUrlParser: true, useUnifiedTopology: true}, ()=>{
+    console.log("It works");
+});
+
 app.get("/", (req, res) =>{
-    res.sendFile(__dirname + "/views/index.html");
+    res.render(__dirname + "/views/index");
 });
 
 const PORT = 3000 || process.env.PORT;
